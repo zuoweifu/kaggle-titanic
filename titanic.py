787588d92e114ca03b0b704425d36477465c7226
zuowei fu <fzw961250672@gmail.com>
2018-03-17 17:34:07 -0400

    submission

--- titanic.py
+++ titanic.py
@@ -1,75 +1,134 @@
-#import libraries
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+#data analysis libraries 
+import numpy as np
 import pandas as pd
 
-#import data
-X = pd.read_csv("train.csv")
-y = X.pop("Survived")
+#ignore warnings
+import warnings
+warnings.filterwarnings('ignore')
 
-#print out initial data
-X.describe()
+#import train and test CSV files
+train = pd.read_csv("train.csv")
+test = pd.read_csv("test.csv")
 
-#claculate mean of age
-X["Age"].fillna(X.Age.mean(),inplace=True)
-X.describe()
+#take a look at the training data
+train.describe(include="all")
 
-#rule out all non-numerical variables
-numerica_varialbles= list(X.dtypes[X.dtypes != object].index)
-X[numerica_varialbles].head()
+#we'll start off by dropping the Cabin feature since not a lot more useful information can be extracted from it.
+train = train.drop(['Cabin'], axis = 1)
+test = test.drop(['Cabin'], axis = 1)
+
+#we can also drop the Ticket feature since it's unlikely to yield any useful information
+train = train.drop(['Ticket'], axis = 1)
+test = test.drop(['Ticket'], axis = 1)
+
+#now we need to fill in the missing values in the Embarked feature
+print("Number of people embarking in Southampton (S):")
+southampton = train[train["Embarked"] == "S"].shape[0]
+print(southampton)
+
+print("Number of people embarking in Cherbourg (C):")
+cherbourg = train[train["Embarked"] == "C"].shape[0]
+print(cherbourg)
+
+print("Number of people embarking in Queenstown (Q):")
+queenstown = train[train["Embarked"] == "Q"].shape[0]
+print(queenstown)
+
+#replacing the missing values in the Embarked feature with S
+train = train.fillna({"Embarked": "S"})
+
+#create a combined group of both datasets
+combine = [train, test]
+
+#extract a title for each Name in the train and test datasets
+for dataset in combine:
+    dataset['Title'] = dataset.Name.str.extract(' ([A-Za-z]+)\.', expand=False)
+
+pd.crosstab(train['Title'], train['Sex'])
+
+#replace various titles with more common names
+for dataset in combine:
+    dataset['Title'] = dataset['Title'].replace(['Lady', 'Capt', 'Col',
+    'Don', 'Dr', 'Major', 'Rev', 'Jonkheer', 'Dona'], 'Rare')
+    
+    dataset['Title'] = dataset['Title'].replace(['Countess', 'Lady', 'Sir'], 'Royal')
+    dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
+
+train[['Title', 'Survived']].groupby(['Title'], as_index=False).mean()
+
+#map each of the title groups to a numerical value
+title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Royal": 5, "Rare": 6}
+for dataset in combine:
+    dataset['Title'] = dataset['Title'].map(title_mapping)
+    dataset['Title'] = dataset['Title'].fillna(0)
+
+train.head()
 
 
-model = RandomForestRegressor(n_estimators=100,oob_score=True, random_state=42)
-model.fit(X[numerica_varialbles],y)
-model.oob_score_
-y_oob = model.oob_prediction_
-print "c-stat: ",roc_auc_score(y,y_oob)
-'''
-the result is 0.73995515504
-'''
+
+#dropping the Age feature for now, might change
+train = train.drop(['Age'], axis = 1)
+test = test.drop(['Age'], axis = 1)
+
+#drop the name feature since it contains no more useful information.
+train = train.drop(['Name'], axis = 1)
+test = test.drop(['Name'], axis = 1)
+#map each Sex value to a numerical value
+sex_mapping = {"male": 0, "female": 1}
+train['Sex'] = train['Sex'].map(sex_mapping)
+test['Sex'] = test['Sex'].map(sex_mapping)
+
+train.head()
+
+#map each Embarked value to a numerical value
+embarked_mapping = {"S": 1, "C": 2, "Q": 3}
+train['Embarked'] = train['Embarked'].map(embarked_mapping)
+test['Embarked'] = test['Embarked'].map(embarked_mapping)
+
+train.head()
+
+#fill in missing Fare value in test set based on mean fare for that Pclass 
+for x in range(len(test["Fare"])):
+    if pd.isnull(test["Fare"][x]):
+        pclass = test["Pclass"][x] #Pclass = 3
+        test["Fare"][x] = round(train[train["Pclass"] == pclass]["Fare"].mean(), 4)
+        
+#map Fare values into groups of numerical values
+train['FareBand'] = pd.qcut(train['Fare'], 4, labels = [1, 2, 3, 4])
+test['FareBand'] = pd.qcut(test['Fare'], 4, labels = [1, 2, 3, 4])
+
+#drop Fare values
+train = train.drop(['Fare'], axis = 1)
+test = test.drop(['Fare'], axis = 1)
+
+#check train data
+train.head()
+
+#check test data
+test.head()
+
+predictors = train.drop(['Survived', 'PassengerId'], axis=1)
+target = train["Survived"]
+x_train, x_val, y_train, y_val = train_test_split(predictors, target, test_size = 0.22, random_state = 0)
+
+# Random Forest
 
 
-def describe_categorical(X):
-    from IPython.display import display, HTML
-    display(HTML(X[X.columns[X.dtypes == "object"]].describe().to_html()))
+randomforest = RandomForestClassifier()
+randomforest.fit(x_train, y_train)
+y_pred = randomforest.predict(x_val)
+acc_randomforest = round(accuracy_score(y_pred, y_val) * 100, 2)
+print(acc_randomforest)
 
-describe_categorical(X)
+#set ids as PassengerId and predict survival 
+ids = test['PassengerId']
+predictions = randomforest.predict(test.drop('PassengerId', axis=1))
 
-'''
-througn observation, i found that name, ticket and PassengerId might not be useful for the model
-so just droped them
-'''
-X.drop(["Name","Ticket","PassengerId"],axis=1,inplace=True)
-
-'''
-only keep the first cahr of cabin
-'''
-def clean_cabin(x):
-    try:
-        return x[0]
-    except TypeError:
-        return "None"
-
-X["Cabin"] = X.Cabin.apply(clean_cabin)
-
-X.Cabin
-
-categorical_variables = ['Sex','Cabin','Embarked']
-for variable in categorical_variables:
-    X[variable].fillna("Missing",inplace=True)
-    dummies = pd.get_dummies(X[variable],prefix=variable)
-    X = pd.concat([X,dummies],axis=1)
-    X.drop([variable],axis=1,inplace=True)
-
-def printall(X,max_rows=10):
-    from IPython.display import display,HTML
-    display(HTML(X.to_html(max_rows=max_rows)))
-
-printall(X)
-
-model = RandomForestRegressor(100,oob_score=True, n_jobs=-1,random_state=42)
-model.fit(X,y)
-print "c-stat: ", roc_auc_score(y,model.oob_prediction_)
-'''
-the result is 0.863521128261
-'''
+#set the output as a dataframe and convert to csv file named submission.csv
+output = pd.DataFrame({ 'PassengerId' : ids, 'Survived': predictions })
+output.to_csv('submission.csv', index=False)
